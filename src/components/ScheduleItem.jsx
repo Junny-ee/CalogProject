c5c67d28bb2c8a9210ac09e60880f0c61bb5e9d4
hyunho <smz1722@naver.com>
2025-06-17 19:26:52 +0900

    버튼 오류 수정, 모달윈도우 컴포넌트 추가(모달용)

--- src/components/ScheduleItem.jsx
+++ src/components/ScheduleItem.jsx
@@ -1,15 +1,27 @@
-import { useState } from "react";
 import "./ScheduleItem.css";
-
+import { useState } from "react";
+import ReactModal from "react-modal";
+import Button from "./Button";
+import ModalWindow from "./ModalWindow";
 const mockdata = {
   isChecked: false,
   content: "제목",
 };
 const ScheduleItem = () => {
-  const [Schedule, setSechdule] = useState();
+  const [Schedule, setSechdule] = useState(mockdata); // 목데이터(나중에는 글 객체)
+  const onCreate = () => {};
   return (
     <div className="ScheduleItem">
-      <h4>할일 목록</h4>
+      <div className="ScheduleWirte">
+        <h4 style={{ display: "inline" }}>할일 목록</h4>
+        <ModalWindow />
+      </div>
+      <div>
+        <div>
+          <input type="checkbox" className="checkBox" onClick={onCreate} />
+          <a href="">{Schedule.content}</a>
+        </div>
+      </div>
     </div>
   );
 };
