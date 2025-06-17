7d4f0ea425a520006fdd4eafc7a6bd960b159eff
jihyun <jhbuse0612@gmail.com>
2025-06-17 15:20:43 +0900

    Calendar.css 추가

--- src/pages/New.jsx
+++ src/pages/New.jsx
@@ -1,5 +1,17 @@
+import Button from "../components/Button";
 const New = () => {
-  return <div className="New">생성페이지</div>;
+  return (
+    <div className="New">
+      <div className="title_content">글 제목</div>
+      <div className="date_content">날짜</div>
+      <div className="write_content">글 작성</div>
+      <div className="write_content">
+        <Button text={"수정"} classtype={"Create"} />
+        <Button text={"삭제"} />
+        <Button text={"작성 완료"} />
+      </div>
+    </div>
+  );
 };
 
 export default New;
