19a5787a5dc363cb4df14dc186833a1acc969ddb
LCW <lcwsky28@gmail.com>
2025-06-19 17:36:33 +0900

    로컬 스토리지 적용/삭제 구현

--- src/pages/Read.jsx
+++ src/pages/Read.jsx
@@ -2,11 +2,9 @@ import { useContext } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { postContent } from "../util/postContent";
 import "./Read.css";
-import { CalogStateContext } from "../App";
+import { CalogDispatchContext, CalogStateContext } from "../App";
 const Read = ({ title, createDate, content, tag }) => {
-  const targetContent = postContent.find(
-    (content) => Number(content.id) === Number(params.id)
-  );
+  const { onDelete } = useContext(CalogDispatchContext);
   const nav = useNavigate();
   const params = useParams();
   const contentList = useContext(CalogStateContext);
@@ -18,19 +16,19 @@ const Read = ({ title, createDate, content, tag }) => {
     <>
       <button onClick={() => nav(-1)}>뒤로가는 버튼</button>
       <button onClick={() => nav(`/edit/${params.id}`)}>수정하기</button>
-      <button>삭제하기(미구현)</button>
-      <h1>{targetContent.title}</h1>
+      <button onClick={() => { onDelete(params.id); nav("/backboard") }}>삭제하기</button>
+      <h1>{rightNum.title}</h1>
       <div className="read_createDate">
-        {new Date(targetContent.createDate).toLocaleDateString()}
+        {new Date(rightNum.createDate).toLocaleDateString()}
       </div>
-      <div className="read_content">{targetContent.content}</div>
+      <div className="read_content">{rightNum.content}</div>
       {/* 글 상세보기 창에서 태그 클릭시 태그별 검색 작동 필요*/}
       <div
         className="read_tag"
         onClick={() => {
           nav(-1);
         }}
-      >{`#${targetContent.tag}`}</div>
+      >{`#${rightNum.tag}`}</div>
       {/* <div className="Read">
         <div className="read_title_content">{rightNum.title}</div>
         <div className="read_tag_content">{spreadTag}</div>
