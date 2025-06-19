2bb8962848c5a7038196e1f0e8d132cfa89bc7ee
boomi <laxpunzel@gmail.com>
2025-06-19 16:02:47 +0900

    0619 FrontCalendar 날짜 props 넘기기 수정

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,30 @@ import New from "./pages/New";
 import Read from "./pages/Read";
 import Notfound from "./pages/Notfound";
 import BackBoardMain from "./components/BackBoardMain";
+import FrontCalendar from "./components/FrontCalendar";
+import { CalendarProvider } from "./components/FrontCalendar";
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Calendar />} />
-        <Route path="/new" element={<New />} />
-        <Route path="/backboard" element={<BackBoardMain />} />
-        <Route path="/edit/:id" element={<Edit />} />
-        <Route path="/read/:id" element={<Read />} />
-        <Route path="/*" element={<Notfound />} />
-      </Routes>
+      <CalendarProvider>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <FrontCalendar />
+              </>
+            }
+          />
+          <Route path="/" element={<Calendar />} />
+          <Route path="/new" element={<New />} />
+          <Route path="/backboard" element={<BackBoardMain />} />
+          <Route path="/edit/:id" element={<Edit />} />
+          <Route path="/read/:id" element={<Read />} />
+          <Route path="/*" element={<Notfound />} />
+        </Routes>
+      </CalendarProvider>
     </>
   );
 }
--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -1,7 +1,33 @@
 import "./Summary.css";
+import Button from "./Button";
+import SummaryItem from "./SummaryItem";
+import { useNavigate } from "react-router-dom";
+import { postContent } from "../util/postContent";
+const Summary = ({ date }) => {
+  const nav = useNavigate();
+  console.log("선택한 날짜:", date);
 
-const Summary = () => {
-  return <div className="Summary">요약창 구역</div>;
+  const check = new Date("2025-06-26");
+  // const today = Date(date); 추후 데이터 받으면 넣기
+  const todaySummary = postContent.filter(
+    (date) => date.createDate === check.getTime()
+  ); // 날짜 일치하는 객체 가져오기
+  // console.log(check.toLocaleDateString());
+  return (
+    <div className="Summary">
+      <div className="Summary_Header">
+        {/* <h4>{check.toLocaleDateString()}</h4> 나중에 데이터받으면 수정 */}
+        {/* 렌더링 확인 코드  */}
+        <h4>
+          {date ? new Date(date).toLocaleDateString() : "날짜를 선택하세요"}
+        </h4>
+        <Button text={"+"} classtype={"Create"} onClick={() => nav("/new")} />
+      </div>
+      <div className="Summary_Contests">
+        <SummaryItem item={todaySummary} />
+      </div>
+    </div>
+  );
 };
 
 export default Summary;
