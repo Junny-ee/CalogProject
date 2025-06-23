5d41ec24c7f3d6f6c69c1d3992be5b361415cb36
jihyun <jhbuse0612@gmail.com>
2025-06-23 17:45:31 +0900

    아이콘 수정

--- src/components/BackPostItem.jsx
+++ src/components/BackPostItem.jsx
@@ -5,7 +5,15 @@ import { useContext, useState, useEffect } from "react";
 import { CalogDispatchContext } from "../App";
 
 // 태그 여러 개 구현 필요
-const BackPostItem = ({ id, title, createDate, content, tag, checkedItems, checkedItemHandler }) => {
+const BackPostItem = ({
+  id,
+  title,
+  createDate,
+  content,
+  tag,
+  checkedItems,
+  checkedItemHandler,
+}) => {
   const nav = useNavigate();
   const { setSearchWord, setSearchingTag, setShowSearchBar } = useContext(
     BackBoardDispatchContext
@@ -27,21 +35,31 @@ const BackPostItem = ({ id, title, createDate, content, tag, checkedItems, check
   const { onDelete } = useContext(CalogDispatchContext);
   const [isChecked, setIsChecked] = useState(checkedItems.includes(id));
   const check = ({ target }) => {
-    checkedItemHandler(target.value, target.checked)
-    setIsChecked(target.checked)
-  }
+    checkedItemHandler(target.value, target.checked);
+    setIsChecked(target.checked);
+  };
 
   useEffect(() => {
     if (checkedItems.includes(id)) {
-      setIsChecked(true)
+      setIsChecked(true);
     } else {
-      setIsChecked(false)
+      setIsChecked(false);
     }
-  }, [checkedItems, id])
+  }, [checkedItems, id]);
   return (
     <div className="post_item" onClick={() => nav(`/read/${id}`)}>
       <div className="contents_wrapper">
         <div className="content_header">{title}</div>
+        <div className="content_date">
+          {new Date(createDate).toLocaleDateString()}
+        </div>
+        <div className="content_body">
+          {" "}
+          {markdownPreviewLines().map((line, idx) => (
+            <p key={idx}>{line}</p>
+          ))}
+        </div>
+
         <div className="content_tag">
           {tag &&
             tag.map((item, index) => (
@@ -58,15 +76,6 @@ const BackPostItem = ({ id, title, createDate, content, tag, checkedItems, check
                 {`#${item}`}
               </div>
             ))}
-        </div>
-        <div className="content_body">
-          {" "}
-          {markdownPreviewLines().map((line, idx) => (
-            <p key={idx}>{line}</p>
-          ))}
-        </div>
-        <div className="content_date">
-          {new Date(createDate).toLocaleDateString()}
         </div>
       </div>
       <input
